6ff0e19e8cee12c32ae4f73679f6aa416b4e89d6
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-03-25 13:44:17 +0800

    Create a function that take a location
    Use fetch with the url of an api plus with the location
    Return json file so they can be use for the app.
    
    Create a function that can take a location and weatherapi within
    the object, and second object made for elements to replace the
    default value of html elements with the return values of the weather
    api.

---
+++ src/js/weatherInfo.js
@@ -0,0 +1,78 @@
+const weatherAPI = ({ location  }) => {
+  const url = `https://api.weatherapi.com/v1/current.json?key=53a730f3a4b74aefa2144040241703&q=${location}`;
+  
+  const getWeather = async () => {
+    const resources = await fetch(url, { mode: 'cors' });
+    const resolve = await resources.json();
+
+    return resolve;
+  }
+  
+  return { getWeather };
+};
+
+const weatherInfo = ({
+    location, 
+    weatherApi, 
+  }, {
+    locationNameStr,
+    locationDateStr,
+    tempCStr,
+    conditionStr, 
+    conditionImg,
+    uvStr, 
+    feelsLikeStr, 
+    humidityStr, 
+    windDirStr, 
+    windKphStr, 
+    pressureMbStr, 
+    visKmStr
+  }) => {
+  const locationName = document.querySelector(locationNameStr);
+  const locationDate = document.querySelector(locationDateStr);
+  const tempC = document.querySelector(tempCStr);
+  const conditionText = document.querySelector(conditionStr);
+  const conditionImage = document.querySelector(conditionImg);
+
+  const uvText = document.querySelector(uvStr);
+  const feelsLikeText = document.querySelector(feelsLikeStr);
+  const humidityText = document.querySelector(humidityStr);
+  const windDirText = document.querySelector(windDirStr);
+  const windKphText = document.querySelector(windKphStr);
+  const pressureMbText = document.querySelector(pressureMbStr);
+  const visKmText = document.querySelector(visKmStr);
+ 
+  const getInfo = () => {
+    const weatherInfoGetter = weatherApi({ location }).getWeather()
+      .then((resolve) => {
+        const current = resolve['current'];
+        const locationInfo = resolve['location'];
+
+        locationName.innerText = `${locationInfo.name}, ${locationInfo.country}`;
+        tempC.innerText = `${current.temp_c} C`;
+        conditionText.innerText = `${current.condition.text}`; 
+        conditionImage.setAttribute('src', `https:${current.condition.icon}`);
+        locationDate.innerText = `Last Updated: ${current.last_updated}`;
+
+        uvText.innerText = `${current.uv}`; 
+        feelsLikeText.innerText = `${current.feelslike_c} C`;
+        humidityText.innerText = `${current.humidity}%`;
+        windDirText.innerText = `${current.wind_dir}`;
+        windKphText.innerText = `${current.wind_kph}`;
+        pressureMbText.innerText = `${current.pressure_mb}`;
+        visKmText.innerText = `${current.vis_km}`;
+        
+        if (resolve['error']) {
+          console.log(resolve['error'].message);
+        }
+      })
+      .catch((err) => console.log(err)); 
+
+    return weatherInfoGetter;
+  };
+
+  return { getInfo };
+};
+
+ export { weatherAPI, weatherInfo };
+   
